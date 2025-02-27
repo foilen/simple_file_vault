787b46a4484b96b08722fac545951832ddf8bc57
Simon Levesque <simon@foilen.com>
2025-02-27 18:03:27 -0500

    Spring Init

---
+++ src/main/java/com/foilen/simple_file_vault/SimpleFileVaultApplication.java
@@ -0,0 +1,13 @@
+package com.foilen.simple_file_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFileVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFileVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foilen/simple_file_vault/SimpleFileVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foilen.simple_file_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFileVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
